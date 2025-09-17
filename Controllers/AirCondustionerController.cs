fe5599e56d57ad25f9f6c963b6a9b8aed4bb047b
Junaid-12 <77788942+Junaid-12@users.noreply.github.com>
2025-09-17 21:48:35 +0530

    First Commit

---
+++ Controllers/AirCondustionerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcRepair.Models;
+
+namespace AcRepair.Controllers
+{
+    public class AirCondustionerController : Controller
+    {
+        // GET: AirCondustioner
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(UserModel md)
+        {
+            AppllicationDbContext db = new AppllicationDbContext();
+            db.UserAppoimentdata(md);
+            return View();
+        }
+    
+   
+    }
+}
---
+++ Models/AppllicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AcRepair.Models
+{
+    public class AppllicationDbContext
+    {
+        SqlConnection con;
+        public AppllicationDbContext()
+        {
+       
+               con = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+        }
+
+        public bool UserAppoimentdata(UserModel md)
+        {
+            string query = "Insert Into usercurd (Name,Email,Description)values('"+md.Name+"','"+md.Email+"','"+md.Description+"')";
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (1 > 0) { return true; } 
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
